b5864d4be8e9aa014676eab244c6fad60b33fce8
Danny White <hi@dannywhite.org>
2023-05-26 11:10:29 +1000

    feat: relative date formatter

--- .eleventy.js
+++ .eleventy.js
@@ -3,31 +3,40 @@ const { DateTime } = require("luxon");
 const { srcset, src } = require("./src/helpers/shortcodes");
 
 module.exports = function (config) {
+    // Copy folders into output directory
     config.addPassthroughCopy("./src/style.css")
+    config.addPassthroughCopy("./src/js/");
 
     // Shortcodes
     // Image shortcodes from helpers/shortcodes.js
     config.addShortcode("src", src)
     config.addShortcode("srcset", srcset)
 
-    // Filters
-    // Date filter for last updated date on guides
-    // https://11ty.rocks/eleventyjs/dates/#postdate-filter
-    // https://moment.github.io/luxon/docs/class/src/datetime.js~DateTime.html
-    config.addFilter("guideDate", (dateObj) => {
-        // return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED);
-        // Actually just using same format as `generated` for now...
-        return new Intl.DateTimeFormat(
-            'en-GB', { dateStyle: 'full' }
-        ).format(dateObj);
-    });
-
-    config.addGlobalData('generated', () => {
+    // Global data
+    config.addGlobalData('generatedDate', () => {
         let now = new Date();
         return new Intl.DateTimeFormat(
             'en-GB', { dateStyle: 'full' }
         ).format(now);
     });
+
+    // Filters
+    // Date filter for ISO8601 format (for further manipulation in JavaScript)
+    // https://www.aleksandrhovhannisyan.com/blog/useful-11ty-filters/#3-date-formatting
+    const toISOString = (dateString) => new Date(dateString).toISOString();
+    config.addFilter('toISOString', toISOString);
+
+    // Date filter for last updated date on guides
+    // https://11ty.rocks/eleventyjs/dates/#postdate-filter
+    // https://moment.github.io/luxon/docs/class/src/datetime.js~DateTime.html
+    config.addFilter("readableDate", (dateObj) => {
+        // return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED);
+        // Actually just using same format as `generatedDate` for now...
+        return new Intl.DateTimeFormat(
+            'en-GB', { dateStyle: 'full' }
+        ).format(dateObj);
+    });
+
 
     // Make a collection of guides but only those that aren't marked as 'draft'
     config.addCollection("publicGuides", function (collectionApi) {
