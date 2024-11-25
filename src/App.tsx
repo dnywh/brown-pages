e6983841ee6a34e0384ee97a4b2659dfa9dd6dd7
Danny White <3104761+dnywh@users.noreply.github.com>
2024-11-25 11:19:19 +0100

    First commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-6xl font-bold underline bg-sky-200">Hello world!</h1>
+    </>
+  );
+}
+
+export default App;
---
+++ src/firebase.ts
@@ -0,0 +1,18 @@
+import { initializeApp, FirebaseApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
+
+// Firebase configuration object
+const firebaseConfig: Record<string, string> = {
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+};
+
+// Initialize Firebase
+const app: FirebaseApp = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
+
+export default app;
