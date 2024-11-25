211196b6f4a1f26056e5492d135273a8d3dbb884
Danny White <3104761+dnywh@users.noreply.github.com>
2024-11-25 17:38:58 +0100

    Marked coordinates as required

--- src/types/Host.ts
+++ src/types/Host.ts
@@ -3,8 +3,8 @@ export interface Host {
   name: string;
   lastActive: string;
   locationName: string;
-  latitude?: number; // Use optional fields where not always required
-  longitude?: number;
+  latitude: number; // Use optional fields where not always required
+  longitude: number;
   details: string;
   accepted: string[]; // Array of accepted items
   notAccepted: string[]; // Array of not accepted items
