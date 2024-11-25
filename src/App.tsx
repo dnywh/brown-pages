39553ab3cf4f522302820e277a8ce64456471e02
Danny White <3104761+dnywh@users.noreply.github.com>
2024-11-25 11:22:55 +0100

    First push

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,9 @@ import "./App.css";
 function App() {
   return (
     <>
-      <h1 className="text-6xl font-bold underline bg-sky-200">Hello world!</h1>
+      <h1 className="text-6xl font-bold underline bg-sky-200">
+        Hello from Brown Pages
+      </h1>
     </>
   );
 }
